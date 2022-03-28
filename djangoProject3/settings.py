72c5d7c01e933ecba16d984eed8a96687a7f73a0
lgkiemde <74085491+lgkiemde@users.noreply.github.com>
2022-03-27 22:21:44 -0500

    pythonanywhere

--- djangoProject3/settings.py
+++ djangoProject3/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-(7#4reaj+l!6r^%%ln!7@6kl=gbbfo2cd7$@535l5anoog7u(6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
