39aabd73723785f90d1b80db7e4b1f5e0e045cf7
lgkiemde <74085491+lgkiemde@users.noreply.github.com>
2022-03-27 23:12:10 -0500

    Update settings.py

--- djangoProject3/settings.py
+++ djangoProject3/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = Path(BASE_DIR / 'static')
+STATICFILES_DIRS = [ 
+    os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
